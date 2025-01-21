ce0085d4b1b85115a326c8ea20a9c01ae2599747
Luanxiaorui <2801611461@qq.com>
2025-01-21 23:18:41 +0800

    Site updated: 2025-01-21 23:18:40

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1737472036543";const preCache = ["/images/taichi.png","/images/Ocean.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1737472716123";const preCache = ["/images/taichi.png","/images/Ocean.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
